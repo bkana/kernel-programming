d5ea10791c61f25795fad490bc6dc146cf0e847f
B. Nguiemya Kana <nkana@outlook.de>
2017-07-25 17:05:53 +0200

    Passing array Arguments to the Kernel Module

--- Hello.c
+++ Hello.c
@@ -4,15 +4,17 @@
 #include <linux/moduleparam.h>
 
 //step 2 create variable
-int param_var = 0;
+int param_var[3] = {0,0,0};
 
 //step 3 register (macro)
 //module_param(name_var, type, permissions)
 
-module_param(param_var,int,S_IRUSR | S_IWUSR);
+module_param_array(param_var,int,NULL ,S_IRUSR | S_IWUSR);
 void display(void)
 {
-	printk(KERN_ALERT "TEST: param = %d", param_var);
+	printk(KERN_ALERT "TEST: param = %d", param_var[0]);
+        printk(KERN_ALERT "TEST: param = %d", param_var[1]);
+        printk(KERN_ALERT "TEST: param = %d", param_var[2]);
 }
 static int hello_init(void)
 {
